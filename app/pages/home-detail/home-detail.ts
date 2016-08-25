9a3c60bed90890937ec5bea2ed542667ece8d315
Jorge Vergara <javebratt@gmail.com>
2016-08-24 21:34:10 -0500

    Creates apartment detail page

---
+++ app/pages/home-detail/home-detail.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the HomeDetailPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/home-detail/home-detail.html',
+})
+export class HomeDetailPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
