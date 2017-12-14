91e858adbeddff9c732630a64a652168c70fce2d
Jorge Vergara <javebratt@gmail.com>
2017-12-14 16:45:40 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,16 @@
-import { Component, NgZone } from '@angular/core';
+import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import firebase from 'firebase';
-
+import { HomePage } from '../pages/home/home';
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: string;
-  zone: NgZone;
+  rootPage:any = HomePage;
 
-  constructor(platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen) {
-
-    const firebaseConfig = {
-      apiKey: "AIzaSyBwEUe6x_w_yLFrr--xYLQJLxRT2Rc8vtY",
-      authDomain: "ionic-firebase-auth-9f555.firebaseapp.com",
-      databaseURL: "https://ionic-firebase-auth-9f555.firebaseio.com",
-      storageBucket: "ionic-firebase-auth-9f555.appspot.com",
-      messagingSenderId: "904481277327"
-    };
-
-    firebase.initializeApp(firebaseConfig);
-
-    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-      this.zone.run( () => {
-        if (user) {
-          this.rootPage = 'AnonymousListPage';
-          unsubscribe();
-        } else {
-          this.rootPage = 'LandingPage';
-          unsubscribe();
-        }
-      });
-    });
-
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -44,3 +19,4 @@ export class MyApp {
     });
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-
-// Import the providers
-import { AuthProvider } from '../providers/auth/auth';
-
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    HomePage
   ],
   imports: [
+    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    HomePage
   ],
   providers: [
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider,
     StatusBar,
-    SplashScreen
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
